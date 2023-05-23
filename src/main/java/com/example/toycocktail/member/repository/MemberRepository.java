fd060c2b777660d41b2b6dce67f001bee395fe1c
우진 <woojin9812071207@gmail.com>
2023-05-23 21:08:30 +0900

    project init

---
+++ src/main/java/com/example/toycocktail/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.toycocktail.member.repository;
+
+import com.example.toycocktail.member.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
