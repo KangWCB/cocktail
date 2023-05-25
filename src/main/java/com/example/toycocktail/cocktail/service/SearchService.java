e2ea63fd8c359419e4c5aae7b57ee276d1753c03
신봉규 <daoh98@naver.com>
2023-05-25 22:52:54 +0900

    not used import delete

--- src/main/java/com/example/toycocktail/cocktail/service/SearchService.java
+++ src/main/java/com/example/toycocktail/cocktail/service/SearchService.java
@@ -8,13 +8,9 @@ import com.example.toycocktail.cocktail.repository.LiquidRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
 
 @Service
 @Transactional(readOnly = true)
