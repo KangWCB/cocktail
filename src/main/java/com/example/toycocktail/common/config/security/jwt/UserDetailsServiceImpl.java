123b197d4569d2ac4d590e89f4c534b56f23ab4d
우진 <woojin9812071207@gmail.com>
2023-06-09 15:59:53 +0900

    CurrentUser 어노테이션 추가 UserDetails 스펙 수정 UserDetails -> User 상속

--- src/main/java/com/example/toycocktail/common/config/security/jwt/UserDetailsServiceImpl.java
+++ src/main/java/com/example/toycocktail/common/config/security/jwt/UserDetailsServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.toycocktail.common.config.security.jwt;
 import com.example.toycocktail.member.model.Member;
 import com.example.toycocktail.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -15,12 +14,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException { // memberId값 사용
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException { // memberId값 사용
         Member findMember = memberRepository.findById(Long.parseLong(userId))
                 .orElseThrow(() -> new UsernameNotFoundException("해당 아이디의 유저는 존재하지 않습니다."));
         if (findMember != null)
         {
-            UserDetailsImpl userDetails = new UserDetailsImpl(findMember);
+            UserDetails userDetails = new UserDetails(findMember);
             return userDetails;
         }
         return null;
